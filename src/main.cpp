c8e40e1d818eddd66a008f963a1763195764f3c8
Charles McClelland <chip@mcclellands.org>
2024-09-15 09:28:53 +0800

    Initial Setup

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include "lib/AB1805_RK/src/AB1805_RK.h"
+
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
