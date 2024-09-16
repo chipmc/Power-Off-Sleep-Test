1a563a75b984f741526d6f7a758bf67d47cbe6fe
Charles McClelland <chip@mcclellands.org>
2024-09-16 20:59:07 +0800

    Successfull test for all three modes.

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,27 @@ void setup() {
 
   // Log.begin(LOG_LEVEL_SILENT, &Serial);
 	Log.begin(LOG_LEVEL_INFO, &Serial);
-	Log.infoln("See Insights LoRa Node - Power Off Sleep Test");
 
   gpio.setup(); 														        // Setup the pins
 
   ab1805.withFOUT(gpio.WAKE).setup();               // Set up the RTC with WAKE on pin 8
 
+  // This is how to check if we did a deep power down (optional)
+  AB1805::WakeReason wakeReason = ab1805.getWakeReason();  
+  if (wakeReason == AB1805::WakeReason::DEEP_POWER_DOWN) {
+      Log.infoln("Third Test - Success - Woke from DEEP_POWER_DOWN");
+      Log.infoln("*********************************************");
+  }
+  else {
+    Log.infoln("*********************************************");
+	  Log.infoln("See Insights LoRa Node - Power Off Sleep Test");
+  }
+
   LowPower.attachInterruptWakeup(gpio.WAKE, wakeUp_Timer, FALLING);   // Attach the interrupt
 
   { // First Test - Regular Sleep
     Log.infoln("First Test - sleep for 10 seconds");
+    delay(500);                                               // Get the message out before sleeping
     LowPower.sleep(10000);                          // Sleep for 10 seconds
     Log.infoln("Awoke from first test - pause for 5 seconds");
   }
@@ -37,7 +48,8 @@ void setup() {
   delay (5000);                                     // This simply keeps the serial connection active
 
   { // Second Test - Wake on interrupt
-    Log.infoln("Second Test - sleep for 10 seconds and wake on clock");
+    Log.infoln("Second Test - sleep for 10 seconds & wake on clock");
+    delay(500);                                               // Get the message out before sleeping
     ab1805.interruptCountdownTimer(10,false);        // 10 seconds - no clock set required
     LowPower.sleep(12000);                           // Set the alarm for 12 seconds as a backstop
 
@@ -50,7 +62,30 @@ void setup() {
     IRQ_Reason = 0;
   }
 
+  { // Third Test - Deep Power Down Sleep - limited to 255 seconds
+    time_t seconds = 10;
+    time_t time_cv;
+    uint8_t hundrths_cv;
 
+    Log.infoln("Third Test - Power off sleep for %i",seconds);
+    delay(500);                                               // Get the message out before sleeping
+
+    // Commenting this out to test the deepPowerDownTime method
+    // ab1805.deepPowerDown(seconds);
+
+    // Instead of using the deepPowerDown method from the AB1805 library that limits it to 255 seconds, we will use the deepPowerDownTime method to put the device to sleep until a speciifc time in the future. 
+    // Care must be taken to ensure the time is set correctly on the AB1805 before calling this method and that the time is always in the future. Otherwise, you risk going to sleep and never waking up. 
+    if (!ab1805.isRTCSet()) {
+      ab1805.setRtcFromTime((time_t)1, 0);
+    }
+    ab1805.getRtcAsTime(time_cv,hundrths_cv);
+    ab1805.deepPowerDownTime((uint32_t)time_cv + seconds, hundrths_cv);
+    Log.infoln("Third Test - Failure - Did not power off");
+  }
+
+  
+
+  Log.infoln("*********************************************");
 }
 
 void loop() {
