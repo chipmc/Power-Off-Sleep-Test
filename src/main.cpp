ce35d12deff59c833b3a536416ed2d9d0f10c3cd
Charles McClelland <chip@mcclellands.org>
2024-09-16 11:44:33 +0800

    Testing - not waking on interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,62 @@
 #include <Arduino.h>
-#include "lib/AB1805_RK/src/AB1805_RK.h"
+#include <ArduinoLowPower.h>
+#include <ArduinoLog.h>
+#include <AB1805_RK.h>
+#include "pinout.h"
 
+#define IRQ_AB1805 1
+
+AB1805 ab1805(Wire); // Class instance for the the AB1805 RTC
 
 // put function declarations here:
-int myFunction(int, int);
+void wakeUp_Timer();
+
+// Variables - we do not have a time source
+volatile uint8_t IRQ_Reason = 0; 										// 0 - Invalid, 1 - AB1805, 2 - RFM95 DIO0, 3 - RFM95 IRQ, 4 - User Switch, 5 - Sensor
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(115200);												      // Establish Serial connection if connected for debugging
+	delay(2000);                                      // Let the serial connect
+
+  // Log.begin(LOG_LEVEL_SILENT, &Serial);
+	Log.begin(LOG_LEVEL_INFO, &Serial);
+	Log.infoln("See Insights LoRa Node - Power Off Sleep Test");
+
+  gpio.setup(); 														        // Setup the pins
+
+  ab1805.withFOUT(gpio.WAKE).setup();               // Set up the RTC with WAKE on pin 8
+
+  LowPower.attachInterruptWakeup(gpio.WAKE, wakeUp_Timer, FALLING);   // Attach the interrupt
+
+  { // First Test - Regular Sleep
+    Log.infoln("First Test - sleep for 10 seconds");
+    LowPower.sleep(10000);                          // Sleep for 10 seconds
+    Log.infoln("Awoke from first test - pause for 10 seconds");
+  }
+
+  delay (10000);                                     // This simply keeps the serial connection active
+
+  { // Second Test - Wake on interrupt
+    Log.infoln("Second Test - sleep for 10 seconds - wake on clock (WAKE pin = %s)", (digitalRead(gpio.WAKE)?"High":"Low"));
+    ab1805.interruptCountdownTimer(10,false);        // 10 seconds - no clock set required
+    LowPower.sleep(12000);                           // Set the alarm for 12 seconds as a backstop
+
+    if (IRQ_Reason == IRQ_AB1805) {
+      Log.infoln("Woke on clock - success (WAKE pin = %s)", (digitalRead(gpio.WAKE)) ? "High":"Low");         // Woke by clock on WAKE pin
+    }
+    else {
+      Log.infoln("Woke on timeout - failure (WAKE pin = %s)", (digitalRead(gpio.WAKE)) ? "High":"Low");       // Woke by the timeout
+    }
+    IRQ_Reason = 0;
+  }
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ab1805.loop();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void wakeUp_Timer() {
+    IRQ_Reason = IRQ_AB1805;                          // Just so we know why we woke
 }
---
+++ src/pinout.cpp
@@ -0,0 +1,38 @@
+#include "pinout.h"
+
+pinout *pinout::_instance;
+
+// [static]
+pinout &pinout::instance() {
+    if (!_instance) {
+        _instance = new pinout();
+    }
+    return *_instance;
+}
+
+pinout::pinout() {
+}
+
+pinout::~pinout() {
+}
+
+void pinout::setup() {
+  //Setup pins to be ins or outs. 
+  pinMode(STATUS,OUTPUT);
+  pinMode(RFM95_CS,OUTPUT);
+  pinMode(RFM95_DIO0,INPUT_PULLUP);
+  pinMode(RFM95_RST, OUTPUT);
+  pinMode(WAKE, INPUT_PULLUP);
+  pinMode(I2C_INT,INPUT_PULLDOWN);
+  pinMode(I2C_EN,OUTPUT);                                   // Not sure if we can use this - Need to test as this might mess with the i2c bus
+  digitalWrite(I2C_EN, HIGH);                               // Turns on the production module - change to LOW if we are using a pre-production module
+  pinMode(BATTINT,INPUT_PULLUP);                            // Battery interrupt pin    
+  pinMode(USER_SW,INPUT_PULLUP);                            // User switch pin
+  
+  //Establish a random seed based on the tank level pin
+  randomSeed(analogRead(Test1));
+}
+
+void pinout::loop() {
+    // Put your code to run during the application thread loop here
+}
